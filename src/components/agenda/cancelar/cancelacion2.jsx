475ac2fc422073039f7d03ee7f1425eb0ab51f6c
felipe <andres_lopez24121@elpoli.edu.co>
2020-11-21 19:18:12 -0500

    Se agrega función para eliminar citas

--- src/components/agenda/cancelar/cancelacion2.jsx
+++ src/components/agenda/cancelar/cancelacion2.jsx
@@ -13,10 +13,10 @@ import Swal from 'sweetalert2';
 const Cancelacion = (props) => {
   const { register, errors, handleSubmit, setError, clearError } = useForm();
   const [citas, setCitas] = useState([]);
+  const [citas2, setCitas2] = useState([]);
 
   const consultarCitas = (data) => {
     let email = data.email;
-
     fetch(`http://localhost:8080/citas/consultarCitasUser/${email}`, {
       method: 'GET',
     }).then(res => res.json()).
@@ -35,6 +35,37 @@ const Cancelacion = (props) => {
     return datos;
   }
 
+  const eliminar = (dato) => {
+    Swal.fire({
+      title: 'Esta seguro de cancelar la cita?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: `Si`,
+      denyButtonText: `No`,
+      customClass: {
+        cancelButton: 'order-1 right-gap',
+        confirmButton: 'order-2',
+        denyButton: 'order-3',
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let contador = 0;
+        let registros = citas;
+        registros.map((registro) => {
+          if (registro.date == dato.date) {
+            registros.splice(contador, 1)
+          }
+          contador++;
+        });
+        setCitas2(registros);
+        console.log(registros);
+      } else if (result.isDenied) {
+        Swal.fire('La cita no se cancelo', '', 'info')
+      }
+    })
+  }
+
+
   const renderCitas = (data, index) => {
     let fecha = new Date(data.date);
     fecha.toLocaleDateString();
@@ -43,7 +74,7 @@ const Cancelacion = (props) => {
         <td>{fecha.toLocaleDateString()}</td>
         <td>{data.hour}</td>
         <td>
-          <Button color="danger" className="btn4">Eliminar</Button>
+          <Button color="danger" className="btn4" onClick={() => eliminar(data)}>Eliminar</Button>
         </td>
       </tr>
     )
@@ -91,7 +122,7 @@ const Cancelacion = (props) => {
             </tr>
           </thead>
           <tbody>
-            {citas.map(renderCitas)}
+            {citas.length != citas2.length ? citas.map(renderCitas) : citas2.map(renderCitas)}
           </tbody>
         </Table>
       </Container>
